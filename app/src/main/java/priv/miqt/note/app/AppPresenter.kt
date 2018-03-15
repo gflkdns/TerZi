8df957a78e6f4d688d9d4a00629f459efe53bd7f
miqt <miqingtang@163.com>
2018-03-15 15:00:39 +0800

    java --> kotlin

---
+++ app/src/main/java/priv/miqt/note/app/AppPresenter.kt
@@ -0,0 +1,16 @@
+package priv.miqt.note.app
+
+import priv.miqt.note.BasePresenter
+
+/**
+ * Created by Administrator on 2018/3/15.
+ */
+class AppPresenter : BasePresenter<AppView> {
+    override fun setView(view: AppView) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun start() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
